a2772724d63ccba2eb5be7f0113a2f0fa226d7b3
Haishgoel <harishgoel636@gmail.com>
2024-06-14 17:54:14 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+const InputField = () => {
+
+    return (
+        <div className="h-screen flex flex-col items-center justify-center bg-gray-400">
+         <h1 className="text-3xl font-bold mb-8">Add Calculator</h1>
+        </div>
+    );
+};
+
+export default InputField;
